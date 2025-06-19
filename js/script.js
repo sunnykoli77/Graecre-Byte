3e39cce8aec083d35b49a6cc7214f9830c8569f9
sunnykoli77 <sunnykoli77@gmail.com>
2025-06-19 22:08:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,18 +15,21 @@ document.querySelectorAll("section[data-section-id]").forEach((section) => {
 });
 
 
-const track = document.querySelector('.imageTrack');
-const images = track.querySelectorAll('picture');
-let currentIndex = 0;
-const imageWidth = 180; // updated width
+window.onload = function () {
+  document.querySelectorAll('.imageTrack').forEach((track) => {
+    const images = track.querySelectorAll('picture');
+    let currentIndex = 0;
 
-function showNextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  const offset = -currentIndex * imageWidth;
-  track.style.transform = `translateX(${offset}px)`;
-}
+    function showNextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      const offset = -currentIndex * 150;
+      track.style.transform = `translateX(${offset}px)`;
+    }
 
-setInterval(showNextImage, 3000);
+    setInterval(showNextImage, 3000);
+  });
+};
+
 
 
 
